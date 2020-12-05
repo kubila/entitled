e2d1e2db6fcef93fb7335e7f71450a87cb5a219d
kubila <dolaskyasin@gmail.com>
2020-12-05 22:21:15 +0300

    finish LeaveAllocation

--- Data/LeaveAllocation.cs
+++ Data/LeaveAllocation.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,21 @@ namespace Entitled.Data
 {
     public class LeaveAllocation
     {
+        [Key]
+        public int Id { get; set; }
+
+        public int NumberOfDays { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
     }
 }
