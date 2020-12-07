fe6d1731c972cd271fb085372aa57583a48123bd
kubila <dolaskyasin@gmail.com>
2020-12-07 23:55:38 +0300

    Create LeaveHistory  interface and finish repository

---
+++ Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Entitled.Contracts;
+using Entitled.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entitled.Repository
+{
+    public class LeaveHistoryRepository : ILeaveHistoryRepository
+    {
+        public bool Create(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveHistory FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
