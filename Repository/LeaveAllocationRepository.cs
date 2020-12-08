fa83bf1ec45a009a173f537cd969728c23f14a3f
kubila <dolaskyasin@gmail.com>
2020-12-08 23:36:40 +0300

    Create LeaveAllocation interface and finish repository

---
+++ Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,42 @@
+﻿using Entitled.Contracts;
+using Entitled.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entitled.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocation
+    {
+        public bool Create(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
