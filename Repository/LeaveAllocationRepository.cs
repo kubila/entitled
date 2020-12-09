d1d9d1497a0e04cf33549580483e371bb8d1940c
kubila <dolaskyasin@gmail.com>
2020-12-09 23:54:33 +0300

    Crud methods for LeaveAllocation

--- Repository/LeaveAllocationRepository.cs
+++ Repository/LeaveAllocationRepository.cs
@@ -18,32 +18,37 @@ namespace Entitled.Repository
 
         public bool Create(LeaveAllocation entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveAllocations.Add(entity);
+            return Save();
         }
 
         public bool Delete(LeaveAllocation entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveAllocations.Remove(entity);
+            return Save();
         }
 
         public ICollection<LeaveAllocation> FindAll()
         {
-            throw new NotImplementedException();
+            return _db.LeaveAllocations.ToList();
         }
 
         public LeaveAllocation FindById(int id)
         {
-            throw new NotImplementedException();
+            var leaveAllocation = _db.LeaveAllocations.Find(id);
+            return leaveAllocation;
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            var whatWeGot = _db.SaveChanges();
+            return whatWeGot > 0;
         }
 
         public bool Update(LeaveAllocation entity)
         {
-            throw new NotImplementedException();
+            _db.LeaveAllocations.Update(entity);
+            return Save();
         }
     }
 }
