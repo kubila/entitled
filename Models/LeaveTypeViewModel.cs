a3dde57e0bd9b568a4c9b17218421272d81719c4
kubila <dolaskyasin@gmail.com>
2020-12-10 23:48:34 +0300

    Create LeaveTypeViewModel

---
+++ Models/LeaveTypeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entitled.Models
+{
+    public class CreateLeaveTypeViewModel
+    {
+        [Required]                
+        public string Name { get; set; }
+
+        
+    }
+
+    public class DetailsLeaveTypeViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
