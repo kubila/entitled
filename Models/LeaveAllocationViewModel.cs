f71cddbf67389f3fb519a81623d8f59f2af46ff9
kubila <dolaskyasin@gmail.com>
2020-12-12 02:05:23 +0300

    Create LeaveAllocationViewModel and finish viewmodels

---
+++ Models/LeaveAllocationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Entitled.Models
+{
+    public class LeaveAllocationViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int NumberOfDays { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        
+        public EmployeeViewModel Employee { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        public IEnumerable<SelectListItem> Employees { get; set; }
+
+
+        public DetailsLeaveTypeViewModel LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+    }
+}
